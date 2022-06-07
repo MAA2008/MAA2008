c205bc66f7fc6dfd4e5ff93b4ee895d9dbba756b
MAA2008 <107029460+MAA2008@users.noreply.github.com>
2022-06-07 14:54:15 +0700

    KALKULATOR.py

--- README.py
+++ README.py
@@ -1,1 +1,7 @@
-print("h")
+while(True):
+    angka1=input ("masukkan angka pertama:")
+    angka2=input ("masukkan angka kedua:")
+
+    sum=int(angka1)+int(angka2)
+
+    print("hasil penjumlahan {0}+{1}={2}".format(angka1,angka2,sum))
